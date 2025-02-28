c119452d62eb16475d3cddda67877f79c4e846ea
Cenk Öztürk <ccenkozturrk@outlook.com>
2025-03-01 02:52:57 +0300

    Initialize set-up

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hey bro!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/cemrenezir/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to see the most beautiful thing in the world that has ever happened
         </a>
       </header>
     </div>
