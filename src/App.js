ea1f6b29c923a42bead2b2694b5ff1aff1a1b8b7
Cenk Öztürk <ccenkozturrk@outlook.com>
2025-03-20 19:14:39 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          yo bro!!!
+          yo bro
         </p>
         <a
           className="App-link"
